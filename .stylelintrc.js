ee7ce04c10060fb21c1161cf3ca2c32e22f61957
Baris Igman <bigman924@hotmail.com>
2020-07-01 21:50:04 +0300

    Stylelint changes

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-  "extends": "stylelint-config-standard",
-  "plugins": ["stylelint-scss"],
-  "rules": {
+  extends: 'stylelint-config-standard',
+  plugins: ['stylelint-scss'],
+  rules: {
     "at-rule-no-unknown": null,
     "scss/at-rule-no-unknown": true,
     "no-descending-specificity": null
